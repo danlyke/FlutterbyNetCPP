e4fe1ee6812a6863d0e978e64a60f80135be79ad
Dan Lyke <danlyke@flutterby.com>
2015-01-07 10:29:28 -0800

    Adding some tests, microformats and parsehtml

---
+++ src/test_parsehtml.cpp
@@ -0,0 +1,56 @@
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <locale.h>
+#include "treebuilder.h"
+#include "stringutil.h"
+#include "regexparser.h"
+#include "wiki.h"
+
+
+
+class TestTreeBuilder : public TreeBuilder
+{
+public:
+    TestTreeBuilder() : TreeBuilder() {};
+    virtual ~TestTreeBuilder() {}
+    ParseTreeNode *NodeFactory(const std::string & nodename)
+    {
+        return new ElementNode(nodename);
+    }
+};
+
+
+int main(int argc, char** argv) {
+    TestTreeBuilder treeBuilder;
+
+    setlocale(LC_ALL, ""); /* Use system locale instead of default "C" */
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s regex string\n", argv[0]);
+        return 1;
+    }
+
+    FILE *f = fopen(argv[1], "r");
+    fseek(f, 0L, SEEK_END);
+    size_t length = ftell(f);
+    fseek(f, 0L, SEEK_SET);
+    
+    char *buffer = new char[length + 1];
+    buffer[length] = 0;
+    fread(buffer, length, 1, f);
+    fclose(f);
+
+    length = RemoveCRs(buffer, length);
+
+    {
+        HTMLParser treeParser;
+        treeParser.Parse(treeBuilder, buffer,length);
+    }
+    
+
+    HTMLOutputterString outputter(cout);
+    
+    treeBuilder.AsHTML(outputter);
+    cout << endl;
+    return 0;
+}
