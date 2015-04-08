b6259dcee486647a600d255be7d5fa1c5a8cc4b1
Dan Lyke <danlyke@flutterby.com>
2015-04-08 16:20:22 -0700

    Removing args from main() to remove warnings

--- src/example_httpserver.cpp
+++ src/example_httpserver.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 
 
-int main(int argc, char **argv)
+int main(int /* argc */, char ** /* argv */)
 {
     Net *net = new Net();
 
--- src/example_netserver.cpp
+++ src/example_netserver.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-int main(int argc, char **argv)
+int main(int /* argc */, char ** /* argv */)
 {
     Net *net = new Net();
 
